6a84592ebf6873475f6e02989cc2c0b991fdb291
mogoo7zn <znwangking@hotmail.com>
2025-02-22 11:36:10 +0800

    help scx modify and push four, minor fixes

--- src/features/Home/components/Four/Four.jsx
+++ src/features/Home/components/Four/Four.jsx
@@ -1,51 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import leuk3 from '@assets/images/leukemia3.png';
 import leuk4 from '@assets/images/leukemia4.jpg';
 import './Four.css';
 
 const Four = () => {
+  const [hoveredColumn, setHoveredColumn] = useState(null);
+
+  const handleMouseEnter = (column) => {
+    setHoveredColumn(column);
+  }
+
+  const handleMouseLeave = () => {
+    setHoveredColumn(null);
+  }
+
   return (
     <section id="four">
       <div className="inner">
         <div className="columns">
-          <article  className="alt">
+          <article
+            className={`column ${hoveredColumn === 'left' ? 'hovered' : ''}`}
+            onMouseEnter={() => handleMouseEnter('left')}
+            onMouseLeave={handleMouseLeave}
+          >
             <div className="content">
               <header>
                 <h3>High-Yield L-Asparaginase-Producing E. coli:</h3>
               </header>
               <div className="image fit">
-                <img
-                  src={leuk3}
-                  loading="lazy"
-                  alt="Leukemia Image"
-                  className="responsive-image"
-                />
+                <img src={leuk3} loading="lazy" alt="Leukemia Image" className="responsive-image" />
               </div>
               <p>
-                Researchers have identified E. coli strains capable of high-yield production of L-asparaginase. Optimizing the
-                cultivation conditions of these strains can enhance enzyme production, potentially reducing treatment costs.
+                Researchers have identified E. coli strains capable of high-yield production of
+                L-asparaginase. Optimizing the cultivation conditions of these strains can enhance
+                enzyme production, potentially reducing treatment costs.
               </p>
             </div>
           </article>
 
-          <article>
+          <article
+            className={`column ${hoveredColumn === 'right' ? 'hovered' : ''}`}
+            onMouseEnter={() => handleMouseEnter('right')}
+            onMouseLeave={handleMouseLeave}
+          >
             <div className="content">
               <header>
                 <h3>E. coli as a Probiotic Platform:</h3>
               </header>
               <div className="image fit">
-                <img
-                  src={leuk4}
-                  loading="lazy"
-                  alt="Leukemia Image"
-                  className="responsive-image"
-                />
+                <img src={leuk4} loading="lazy" alt="Leukemia Image" className="responsive-image" />
               </div>
               <p>
-              E. coli is naturally adapted to the human intestinal environment. By engineering E. coli to function as a probiotic, it
-              can produce therapeutic enzymes like L-asparaginase directly in the gut. This approach could offer a cost-effective
-              alternative to traditional enzyme therapy, eliminating the need for continuous injections and potentially improving
-              patient compliance.
+                E. coli is naturally adapted to the human intestinal environment. By engineering E.
+                coli to function as a probiotic, it can produce therapeutic enzymes like
+                L-asparaginase directly in the gut. This approach could offer a cost-effective
+                alternative to traditional enzyme therapy, eliminating the need for continuous
+                injections and potentially improving patient compliance.
               </p>
             </div>
           </article>
